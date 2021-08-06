9418b57b82dc4403d97669130e0fc8d504263820
wali <larts393@gmail.com>
2021-08-06 17:09:52 +0800

    ruby airnote

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+
+    def main
+        @abc = 123
+    end
+
+    def about
+    end
+
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  get "/hello", to: "pages#main"
+  get "/about", to: "pages#about"
+
+  get "/users", to: "users#profile"
+
+end
