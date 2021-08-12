4c4b97bca747207889a242a7ea21bf3bd5319307
wali <larts393@gmail.com>
2021-08-12 11:47:29 +0800

    update

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -9,9 +9,28 @@ class NotesController < ApplicationController
         @note = Note.find(params[:id])
     end
 
+    def edit
+        @note = Note.find(params[:id])
+    end
+
     def new
         @note = Note.new
     end
+
+    def update
+        #Strong Parameter 強參數
+        clean_note = params.require(:note).permit(:title, :content)
+        @note = Note.find(params[:id])
+    
+        if @note.update(clean_note)
+            redirect_to "/notes"
+        else
+             ##待處理
+            render :edit
+            # redirect_to "/notees/new"
+        end
+    end
+
 
     def create
         # render html: params[:note]
@@ -26,8 +45,6 @@ class NotesController < ApplicationController
         # r=params.[:note].permit(:title, :content)
         @note = Note.new(clean_note)
 
-
-
         if @note.save
             redirect_to "/notes"
         else
@@ -36,7 +53,6 @@ class NotesController < ApplicationController
             # redirect_to "/notes/new"
             ##待處理
         end
-        
     end
   
 end
