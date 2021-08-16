b6a3c048676f85db025d0a15134f7fce163f7050
wali <larts393@gmail.com>
2021-08-16 12:02:03 +0800

    sign_up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,23 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to
+        else
         render html: params
+        # 新增users
     end
 
 
     def profile
     end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, 
+                                     :password, 
+                                     :password_confirmation,
+                                     :account)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    
+    attr_accessor :password_confirmation
+
+    vaildates :email, presence:  true
+    vaildates :password, presence:  true
+    vaildates :account, presence:  true
 end
